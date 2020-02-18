ab878d341b6ac1dd2dfa443d984c5b9b441dfa92
Prima Jatnika <primajatnika271995@gmail.com>
2020-02-18 14:33:38 +0700

    Details Prospect Customer

--- lib/src/views/prospect_customer_page/details_prospect_customer.dart
+++ lib/src/views/prospect_customer_page/details_prospect_customer.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:salles_tools/src/utils/hex_converter.dart';
+import 'package:salles_tools/src/utils/screen_size.dart';
 
 class ProspectDetailsView extends StatefulWidget {
   @override
@@ -6,6 +9,8 @@ class ProspectDetailsView extends StatefulWidget {
 }
 
 class _ProspectDetailsViewState extends State<ProspectDetailsView> {
+  ContextType _currentSelectContext;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +28,351 @@ class _ProspectDetailsViewState extends State<ProspectDetailsView> {
         ),
         iconTheme: IconThemeData(color: Colors.black),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            profileImage(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 2),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Text(
+                      "Nama",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 0.3,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 6,
+                    child: Text(
+                      "Bayu Harsono",
+                      style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 2),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Text(
+                      "NIK",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 0.3,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 6,
+                    child: Text(
+                      "3190018751021953",
+                      style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 2),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Text(
+                      "No. Telp",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 0.3,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 6,
+                    child: Text(
+                      "+62 85875074351",
+                      style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 2),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Text(
+                      "Email",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 0.3,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 6,
+                    child: Text(
+                      "bayuharsono@gmail.com",
+                      style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 2),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Text(
+                      "Alamat",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 0.3,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 6,
+                    child: Text(
+                      "Jl. Cicalengka Raya No 11, Antapani Bandung",
+                      style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+              child: FormField(
+                builder: (FormFieldState state) {
+                  return InputDecorator(
+                    decoration: InputDecoration(
+                      hintText: 'Context Type',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    ),
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton<ContextType>(
+//                        value: _currentSelectTask,
+                        hint: Text('Context Type'),
+                        isDense: true,
+                        onChanged: (ContextType newVal) {
+                          setState(() {
+                            _currentSelectContext = newVal;
+                            state.didChange(newVal.contextName);
+                            print(_currentSelectContext.contextName);
+                          });
+                        },
+                        items: ContextType.getTask().map((ContextType val) {
+                          return DropdownMenuItem<ContextType>(
+                            value: val,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(val.contextName),
+                                Text(
+                                  "●",
+                                  style: TextStyle(color: val.contextColor),
+                                ),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 2),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Text(
+                      "Vehicle",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 0.3,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 6,
+                    child: Text(
+                      "Cammry Auto",
+                      style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 2),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Text(
+                      "Warna",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 0.3,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 6,
+                    child: Text(
+                      "Putih",
+                      style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+              child: Container(
+                height: 180,
+                width: screenWidth(context),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage("https://cdn2.tstatic.net/pontianak/foto/bank/images/jual-ktp-palsu_20150414_134332.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              child: Container(
+                width: screenWidth(context),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text("SPK", style: TextStyle(color: Colors.white),),
+                  color: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30),
+              child: Container(
+                width: screenWidth(context),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text("Save", style: TextStyle(color: Colors.white),),
+                  color: HexColor('#E07B36'),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30, bottom: 20),
+              child: Container(
+                width: screenWidth(context),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text("Call", style: TextStyle(color: Colors.white),),
+                  color: Colors.brown,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  Widget profileImage() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 25),
+      child: Center(
+        child: CircleAvatar(
+          backgroundColor: Colors.white,
+          radius: 50,
+          backgroundImage: NetworkImage(
+              "https://content-static.upwork.com/uploads/2014/10/02123010/profilephoto_goodcrop.jpg"),
+        ),
+      ),
+    );
+  }
+}
+
+class ContextType {
+  String contextName;
+  Color contextColor;
+
+  ContextType({this.contextName, this.contextColor});
+
+  static List<ContextType> getTask() {
+    return <ContextType>[
+      ContextType(contextName: 'Context', contextColor: Colors.orangeAccent),
+      ContextType(contextName: 'Prospect', contextColor: Colors.green),
+      ContextType(contextName: 'Hot Prospect', contextColor: Colors.red),
+    ];
   }
 }
