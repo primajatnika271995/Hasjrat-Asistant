d5d9310a21d1eabf830daadd8fcbbf290a8b9f41
Prima Jatnika <primajatnika271995@gmail.com>
2020-02-19 11:24:25 +0700

    Selection Image Profile

--- lib/src/views/profile_page/edit_profile.dart
+++ lib/src/views/profile_page/edit_profile.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:salles_tools/src/utils/hex_converter.dart';
 import 'package:salles_tools/src/utils/screen_size.dart';
 
@@ -8,6 +12,11 @@ class ProfileEditView extends StatefulWidget {
 }
 
 class _ProfileEditViewState extends State<ProfileEditView> {
+  static const String CAMERA_SOURCE = 'CAMERA_SOURCE';
+  static const String GALLERY_SOURCE = 'GALLERY_SOURCE';
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   var namaCtrl = new TextEditingController();
   var tmptTglLahirCtrl = new TextEditingController();
   var jenisKelaminCtrl = new TextEditingController();
@@ -15,6 +24,55 @@ class _ProfileEditViewState extends State<ProfileEditView> {
   var emailCtrl = new TextEditingController();
   var alamatCtrl = new TextEditingController();
   var dealerCtrl = new TextEditingController();
+
+  File _imageSelect;
+
+  void _onPickImageSelected(String source) async {
+    var imageSource;
+    if (source == CAMERA_SOURCE) {
+      imageSource = ImageSource.camera;
+    } else {
+      imageSource = ImageSource.gallery;
+    }
+
+    final scaffold = _scaffoldKey.currentState;
+
+    try {
+      final file = await ImagePicker.pickImage(source: imageSource);
+      if (file == null) {
+        throw Exception('File is not available');
+      }
+
+      final fileCropper = await ImageCropper.cropImage(
+        sourcePath: file.path,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio16x9
+        ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Profile Image Cropper',
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.ratio4x3,
+            showCropGrid: true,
+            toolbarColor: HexColor('#E07B36'),
+            lockAspectRatio: false),
+        iosUiSettings: IOSUiSettings(
+          minimumAspectRatio: 1.0,
+        ),
+      );
+
+      setState(() {
+        _imageSelect = fileCropper;
+      });
+    } catch (e) {
+      scaffold.showSnackBar(SnackBar(
+        content: Text(e.toString()),
+      ));
+    }
+  }
 
   @override
   void initState() {
@@ -32,6 +90,7 @@ class _ProfileEditViewState extends State<ProfileEditView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -58,12 +117,16 @@ class _ProfileEditViewState extends State<ProfileEditView> {
             alamatField(),
             dealerField(),
             Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, top: 50, bottom: 10),
+              padding: const EdgeInsets.only(
+                  left: 20, right: 20, top: 50, bottom: 10),
               child: Container(
                 width: screenWidth(context),
                 child: RaisedButton(
                   onPressed: () {},
-                  child: Text("Save", style: TextStyle(color: Colors.white),),
+                  child: Text(
+                    "Save",
+                    style: TextStyle(color: Colors.white),
+                  ),
                   color: HexColor('#E07B36'),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30),
@@ -88,12 +151,16 @@ class _ProfileEditViewState extends State<ProfileEditView> {
               child: CircleAvatar(
                 backgroundColor: Colors.white,
                 radius: 50,
-                backgroundImage: NetworkImage(
-                    "https://content-static.upwork.com/uploads/2014/10/02123010/profilephoto_goodcrop.jpg"),
+                backgroundImage: _imageSelect == null
+                    ? NetworkImage(
+                        "https://content-static.upwork.com/uploads/2014/10/02123010/profilephoto_goodcrop.jpg")
+                    : FileImage(_imageSelect),
               ),
             ),
             FlatButton(
-              onPressed: () {},
+              onPressed: () {
+                _onPickImageSelected(GALLERY_SOURCE);
+              },
               child: Text(
                 "Change Profile Photo",
                 style: TextStyle(
