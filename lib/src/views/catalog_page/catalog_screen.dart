16f1128e5c020e65dbb39124fae42e077b19af38
Prima Jatnika <primajatnika271995@gmail.com>
2020-02-19 14:45:52 +0700

    Grid Catalog

--- lib/src/views/catalog_page/catalog_screen.dart
+++ lib/src/views/catalog_page/catalog_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:salles_tools/src/utils/hex_converter.dart';
+import 'package:salles_tools/src/views/components/trusty_horizontal_menu.dart';
 
 class CatalogScreen extends StatefulWidget {
   @override
@@ -12,7 +14,7 @@ class _CatalogScreenState extends State<CatalogScreen> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        elevation: 1,
+        elevation: 0,
         titleSpacing: 0,
         title: Text(
           "Catalog",
@@ -21,7 +23,131 @@ class _CatalogScreenState extends State<CatalogScreen> {
             letterSpacing: 0.5,
           ),
         ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.share),
+          ),
+        ],
         iconTheme: IconThemeData(color: Colors.black),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(55),
+          child: Container(
+            height: 55,
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            alignment: Alignment.center,
+            child: TrustyHorizontalMenu(
+              list: [
+                "MPV",
+                "Sedan",
+                "Sport",
+                "Hybrid",
+                "Hatchback",
+                "SUV",
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 13),
+              child: GridView.builder(
+                itemCount: 3,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 3 / 4,
+                  crossAxisSpacing: 17,
+                  mainAxisSpacing: 17,
+                ),
+                itemBuilder: (context, i) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(9.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey[300],
+                          offset: Offset(0, 3),
+                          blurRadius: 3.0,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(9.0),
+                                topRight: Radius.circular(9.0),
+                              ),
+                              color: Color(0xffe5e6ea),
+                            ),
+                            child: Image.network(
+                                "https://m.toyota.astra.co.id/sites/default/files/2019-04/car-pearl.png"),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(9.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "Toyota Camry",
+                                style: Theme.of(context).textTheme.title,
+                              ),
+                              Text(
+                                "2019 Auto",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .subhead
+                                    .apply(color: Colors.grey[500]),
+                              ),
+                              SizedBox(
+                                height: 5.0,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  InkWell(
+                                    onTap: () {},
+                                    child: Container(
+                                      padding: EdgeInsets.all(5.0),
+                                      decoration: BoxDecoration(
+                                        color: HexColor('#E07B36'),
+                                        shape: BoxShape.circle,
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.grey[400],
+                                            blurRadius: 5.0,
+                                            offset: Offset(0, 3),
+                                          ),
+                                        ],
+                                      ),
+                                      child: Icon(
+                                        Icons.visibility,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
