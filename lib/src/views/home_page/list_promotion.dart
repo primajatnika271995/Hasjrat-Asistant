74974eda31971e35ca069aac704e8ac3bbe724e3
Prima Jatnika <primajatnika271995@gmail.com>
2020-02-20 10:21:02 +0700

    Change List Promotion to Carousel Slider

--- lib/src/views/home_page/list_promotion.dart
+++ lib/src/views/home_page/list_promotion.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
@@ -11,16 +12,32 @@ class PromotionListView extends StatefulWidget {
   _PromotionListViewState createState() => _PromotionListViewState();
 }
 
-class _PromotionListViewState extends State<PromotionListView> with TickerProviderStateMixin {
-  AnimationController animationController;
-
+class _PromotionListViewState extends State<PromotionListView>
+    with TickerProviderStateMixin {
   bool loadImage = true;
+  int _currentImg = 0;
+
+  static List<String> imgList = [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  ];
+
+  static List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+
+    return result;
+  }
 
   @override
   void initState() {
     // TODO: implement initState
-    animationController = AnimationController(
-        duration: Duration(milliseconds: 2000), vsync: this);
     Timer(Duration(seconds: 3), () {
       setState(() => loadImage = false);
     });
@@ -30,7 +47,6 @@ class _PromotionListViewState extends State<PromotionListView> with TickerProvid
   @override
   void dispose() {
     // TODO: implement dispose
-    animationController.dispose();
     super.dispose();
   }
 
@@ -38,92 +54,71 @@ class _PromotionListViewState extends State<PromotionListView> with TickerProvid
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 5, bottom: 16),
-      child: Container(
-        height: 160,
-        width: double.infinity,
-        child: loadImage ? _loadingImageAnimation(context) : ListView.builder(
-          padding:
-          const EdgeInsets.only(top: 0, bottom: 0, right: 3, left: 3),
-          itemCount: 4,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            var count = 4;
-            var animation = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
-                parent: animationController,
-                curve: Interval((1 / count) * index, 1.0,
-                    curve: Curves.fastOutSlowIn)));
-            animationController.forward();
-
-            return PromotionView(
-              animation: animation,
-              animationController: animationController,
-              callback: () {},
-            );
-          },
-        ),
-      ),
+      child: loadImage
+          ? Container(
+              height: 160,
+              child: _loadingImageAnimation(context),
+            )
+          : CarouselSlider(
+              items: child,
+              autoPlay: false,
+              enlargeCenterPage: true,
+              viewportFraction: 0.9,
+              autoPlayCurve: Curves.bounceIn,
+              reverse: false,
+              height: 180,
+              enableInfiniteScroll: false,
+              aspectRatio: 2.0,
+            ),
     );
   }
+
+  final List child = map<Widget>(
+    imgList,
+    (index, i) {
+      return Container(
+        margin: EdgeInsets.all(5.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+          child: Stack(children: <Widget>[
+            Image.network(i, fit: BoxFit.cover, width: 1000.0),
+            Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromARGB(200, 0, 0, 0),
+                      Color.fromARGB(0, 0, 0, 0)
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Text(
+                  'No. $index image',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      );
+    },
+  ).toList();
 
   Widget _loadingImageAnimation(BuildContext context) {
     return ListView.builder(
       scrollDirection: Axis.horizontal,
       itemBuilder: (BuildContext context, int index) => PromotionViewShimmer(),
       itemCount: 2,
-    );
-  }
-
-}
-
-class PromotionView extends StatelessWidget {
-  final VoidCallback callback;
-  final AnimationController animationController;
-  final Animation animation;
-
-  PromotionView({Key key, this.callback, this.animationController, this.animation}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: animationController,
-      builder: (context, Widget child) {
-        return FadeTransition(
-          opacity: animation,
-          child: Transform(
-            transform: new Matrix4.translationValues(
-                100 * (1.0 - animation.value), 0.0, 0.0),
-            child: InkWell(
-              splashColor: Colors.transparent,
-              onTap: () {
-                callback();
-              },
-              child: Card(
-                elevation: 0,
-                color: Colors.grey[50],
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      width: 250,
-                      height: 148,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              'https://dummyimage.com/hd1080'),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        );
-      },
     );
   }
 }
@@ -134,27 +129,19 @@ class PromotionViewShimmer extends StatelessWidget {
     return Card(
       elevation: 0,
       color: Colors.grey[50],
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(5)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
       child: Shimmer.fromColors(
         baseColor: Colors.black38,
         highlightColor: Colors.white,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              width: 250,
-              height: 148,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                color: Colors.black12,
-              ),
-            ),
-          ],
+        child: Container(
+          width: 250,
+          height: 160,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.black12,
+          ),
         ),
       ),
     );
   }
 }
-
-
