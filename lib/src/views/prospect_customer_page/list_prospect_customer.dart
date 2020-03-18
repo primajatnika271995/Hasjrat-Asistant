11409da49009d9c48fa03f17aebf29abda8a8340
Prima Jatnika <primajatnika271995@gmail.com>
2020-03-18 11:56:22 +0700

    Lead Data Format

--- lib/src/views/prospect_customer_page/list_prospect_customer.dart
+++ lib/src/views/prospect_customer_page/list_prospect_customer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:intl/intl.dart';
 import 'package:salles_tools/src/bloc/customer_bloc/customer_bloc.dart';
 import 'package:salles_tools/src/bloc/lead_bloc/lead_bloc.dart';
 import 'package:salles_tools/src/bloc/lead_bloc/lead_event.dart';
@@ -15,7 +16,8 @@ import 'package:salles_tools/src/views/prospect_customer_page/details_prospect_c
 
 class ProspectCustomerListView extends StatefulWidget {
   @override
-  _ProspectCustomerListViewState createState() => _ProspectCustomerListViewState();
+  _ProspectCustomerListViewState createState() =>
+      _ProspectCustomerListViewState();
 }
 
 class _ProspectCustomerListViewState extends State<ProspectCustomerListView> {
@@ -30,8 +32,7 @@ class _ProspectCustomerListViewState extends State<ProspectCustomerListView> {
           child: ProspectContactAdd(),
         ),
         transitionDuration: Duration(milliseconds: 150),
-        transitionsBuilder:
-            (_, Animation<double> animation, __, Widget child) {
+        transitionsBuilder: (_, Animation<double> animation, __, Widget child) {
           return Opacity(
             opacity: animation.value,
             child: child,
@@ -46,8 +47,7 @@ class _ProspectCustomerListViewState extends State<ProspectCustomerListView> {
       PageRouteBuilder(
         pageBuilder: (_, __, ___) => ProspectContactDetailsView(value),
         transitionDuration: Duration(milliseconds: 150),
-        transitionsBuilder:
-            (_, Animation<double> animation, __, Widget child) {
+        transitionsBuilder: (_, Animation<double> animation, __, Widget child) {
           return Opacity(
             opacity: animation.value,
             child: child,
@@ -148,14 +148,16 @@ class _ProspectCustomerListViewState extends State<ProspectCustomerListView> {
                   return index >= state.leads.length
                       ? BottomLoader()
                       : SlidableCustomerView(
-                    index: index,
-                    value: state.leads[index],
-                    callback: () {
-                      _onViewDetailsContact(state.leads[index]);
-                    },
-                  );
+                          index: index,
+                          value: state.leads[index],
+                          callback: () {
+                            _onViewDetailsContact(state.leads[index]);
+                          },
+                        );
                 },
-                itemCount: state.hasReachedMax ? state.leads.length : state.leads.length + 1,
+                itemCount: state.hasReachedMax
+                    ? state.leads.length
+                    : state.leads.length + 1,
               );
             }
             return SizedBox();
@@ -177,7 +179,8 @@ class SlidableCustomerView extends StatelessWidget {
   final Function callback;
   final Datum value;
   final int index;
-  SlidableCustomerView({Key key, this.callback, this.value, this.index}): super(key: key);
+  SlidableCustomerView({Key key, this.callback, this.value, this.index})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -187,7 +190,7 @@ class SlidableCustomerView extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Card(
-          elevation: 3,
+          elevation: 5,
           child: ListTile(
             onTap: () {
               this.callback();
@@ -197,27 +200,71 @@ class SlidableCustomerView extends StatelessWidget {
               foregroundColor: Colors.white,
               backgroundImage: NetworkImage("https://content-static.upwork.com/uploads/2014/10/02123010/profilephoto_goodcrop.jpg"),
             ),
-            title: Text("${value.cardName}"),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(right: 230),
-              child: Container(
-                height: 18,
-                width: 50,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Center(
-                  child: Text(
-                    "${value.suspectstatus}",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 9,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
+            title: Padding(
+              padding: const EdgeInsets.only(top: 5, bottom: 5),
+              child: Text(
+                "${value.leadCode}",
+                style: TextStyle(
+                  letterSpacing: 1.0,
+                  fontSize: 12,
+                  color: Colors.grey,
                 ),
               ),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "${value.cardName}",
+                  style: TextStyle(
+                    letterSpacing: 1.0,
+                    fontSize: 13,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  "${value.phone1}",
+                  style: TextStyle(
+                    letterSpacing: 1.0,
+                    fontSize: 11,
+                    color: Colors.grey,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: Container(
+                        height: 18,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "${value.suspectstatus}",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 9,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      value.suspectDate == null ? "No Date" : "${value.suspectDate.day}/${value.suspectDate.month}/${value.suspectDate.year}",
+                      style: TextStyle(
+                        letterSpacing: 1.0,
+                        fontSize: 11,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
