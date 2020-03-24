7a800c94b3215acd6cf00f4456ab801e6df802fe
Prima Jatnika <primajatnika271995@gmail.com>
2020-03-24 13:45:44 +0700

    List Promotion UI

--- lib/src/views/promotion_page/list_promotion.dart
+++ lib/src/views/promotion_page/list_promotion.dart
@@ -12,7 +12,7 @@ class _PromotionListScreenState extends State<PromotionListScreen> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        elevation: 0,
+        elevation: 1,
         titleSpacing: 0,
         title: Text(
           "Promotion",
@@ -22,6 +22,79 @@ class _PromotionListScreenState extends State<PromotionListScreen> {
           ),
         ),
         iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Card(
+                elevation: 5,
+                child: ListTile(
+                  onTap: () {},
+                  title: Padding(
+                    padding: const EdgeInsets.only(top: 5, bottom: 5),
+                    child: Text(
+                      "Paket Kemerdekaan",
+                      style: TextStyle(
+                        letterSpacing: 1.0,
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Potongan Harga Rp. 4.500.000 untuk mobil Agya.",
+                        style: TextStyle(
+                          letterSpacing: 1.0,
+                          fontSize: 13,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Valid Until 2020-03-03",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5, bottom: 5),
+                            child: Container(
+                              height: 18,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Detail",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 9,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
