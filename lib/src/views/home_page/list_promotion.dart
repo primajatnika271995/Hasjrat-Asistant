41a0fd97719a60a69b789a6f5ea5392157db1080
unknown <yakin.ilmal57@gmail.com>
2020-03-31 13:16:11 +0700

    change to carousel builder list_promotion.dart

--- lib/src/views/home_page/list_promotion.dart
+++ lib/src/views/home_page/list_promotion.dart
@@ -87,70 +87,62 @@ class _PromotionListViewState extends State<PromotionListView>
           if (state is ListProgramPenjualanSuccess) {
             print("List promo success");
 
-            return ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: state.value.data.length,
-              itemBuilder: (context, index) {
-                return CarouselSlider.builder(
-                    initialPage: 1,
-                    autoPlay: false,
-                    enlargeCenterPage: true,
-                    viewportFraction: 0.9,
-                    autoPlayCurve: Curves.bounceIn,
-                    reverse: false,
-                    height: 180,
-                    enableInfiniteScroll: false,
-                    aspectRatio: 2.0,
-                    itemCount: state.value.data.length,
-                    itemBuilder: (context, index) {
-                      var data = state.value.data[index];
-                      return GestureDetector(
-                        onTap: () {},
-                        child: Container(
-                          margin: EdgeInsets.all(5.0),
-                          child: ClipRRect(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(5.0)),
-                            child: Stack(children: <Widget>[
-                              Image.network(
-                                  "https://www.mistercarz.com.my/images/promo/2017/toyota.jpg",
-                                  fit: BoxFit.cover,
-                                  width: 1000.0),
-                              Positioned(
-                                bottom: 0.0,
-                                left: 0.0,
-                                right: 0.0,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                      colors: [
-                                        Color.fromARGB(200, 0, 0, 0),
-                                        Color.fromARGB(0, 0, 0, 0)
-                                      ],
-                                      begin: Alignment.bottomCenter,
-                                      end: Alignment.topCenter,
-                                    ),
-                                  ),
-                                  padding: EdgeInsets.symmetric(
-                                      vertical: 10.0, horizontal: 20.0),
-                                  child: Text(
-                                    '${data.programPenjualanName}',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20.0,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
+            return CarouselSlider.builder(
+                initialPage: 1,
+                autoPlay: false,
+                enlargeCenterPage: true,
+                viewportFraction: 0.9,
+                autoPlayCurve: Curves.bounceIn,
+                reverse: false,
+                height: 180,
+                enableInfiniteScroll: false,
+                aspectRatio: 2.0,
+                itemCount: state.value.data.length,
+                itemBuilder: (context, index) {
+                  var data = state.value.data[index];
+                  return GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      margin: EdgeInsets.all(5.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        child: Stack(children: <Widget>[
+                          Image.network(
+                              "https://www.mistercarz.com.my/images/promo/2017/toyota.jpg",
+                              fit: BoxFit.cover,
+                              width: 1000.0),
+                          Positioned(
+                            bottom: 0.0,
+                            left: 0.0,
+                            right: 0.0,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color.fromARGB(200, 0, 0, 0),
+                                    Color.fromARGB(0, 0, 0, 0)
+                                  ],
+                                  begin: Alignment.bottomCenter,
+                                  end: Alignment.topCenter,
                                 ),
                               ),
-                            ]),
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 10.0, horizontal: 20.0),
+                              child: Text(
+                                '${data.programPenjualanName}',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                      );
-                    });
-              },
-            );
+                        ]),
+                      ),
+                    ),
+                  );
+                });
           }
           return SizedBox();
         },
